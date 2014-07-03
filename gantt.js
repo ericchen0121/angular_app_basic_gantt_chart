6784967f0db3b693607ddc1854355acda29bf0c3
Eric Savvy <eric.chen0121@gmail.com>
2014-07-03 16:44:37 -0700

    put data in div and create base angular app

---
+++ gantt.js
@@ -0,0 +1,29 @@
+function GanttCtrl($scope) {
+  $scope.months = [
+  {
+    name: "Jan",
+    startTime: 1388534400,
+    endTime: 1391212800,
+    rent: 1200
+  },
+  {
+    name: "Feb",
+    startTime: 1391212800,
+    endTime: 1393632000,
+    rent: 1500
+  },
+  {
+    name: "March",
+    startTime: 1393632000,
+    endTime: 1396310400,
+    rent: 1100
+  },
+  {
+    name: "April",
+    startTime: 1396310400,
+    endTime: 1398902400,
+    rent: 1000
+  }]
+
+
+}
